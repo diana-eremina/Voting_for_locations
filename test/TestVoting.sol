c0f216517a92bc7038afee3a0b09e3a277f776e0
diana eremina <diana.eremina@virtualhealth.com>
2023-05-17 17:11:19 +0300

    updating tests - they work now

--- test/TestVoting.sol
+++ test/TestVoting.sol
@@ -9,9 +9,7 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/Voting.sol";
   // the smart contract we want you to test
 
-// Set Up the Contract Tests
-contract TestVoting{
-
+contract TestVoting {
     Voting voting = Voting(DeployedAddresses.Voting());
       // we need to initiate the Voting contract to the deployed contract
       // This will be used to call the functions inside of Voting contracts.
@@ -19,23 +17,19 @@ contract TestVoting{
       // * vote - это название функции в этом файле.
 
     uint256 expectedLocationId = 8;
-      // This is the expected voted location for these tests
-      // Например, тест будет голосовать за локацию №8
+      // Объявили перемнную expectedLocationId с типом uint256 и значением  8
+      // (Тест будет голосовать за локацию с id=8)
 
     address expectedVotedLocation = address(this);
       // This will return the address of the voted location.
-      // before we were setting the ID of the location that we are going to test (expectedLocationId)
+      // earlier we set the ID of the location that we are going to test (expectedLocationId)
       // And now we are just setting what is the address of this location in the blockchain.
-}
+      // пока не поняла, откуда система поймет какой адрес надо брать, что такое "address(this)" ?
 
-// Test Function #1
-  // Now we are going to test if the user can really vote the location with ID 8 and if the returned ID matched.
-  // So basically, we are testing the vote function here.
-
+// Test Function #1 - глосуем за локацию
     function testUserCanVoteLocation() public {
         // объявили тестовую функцию
 
-    // вызов функции голосования
         uint256 returnedId = voting.vote(expectedLocationId);
             // uint256 returnedId - объявили переменную returnedId
             // положили в эту переменную то, что возвращает функция vote
@@ -43,43 +37,48 @@ contract TestVoting{
             // voting.vote = function "vote" from contract "voting".
             // expectedLocationId - голосуем за локацию №8 (отправляем параметр expectedLocationId, который задан как 8)
 
-    // проверка функции голосования
         Assert.equal(
-            // сравниваем переменные "returnedId" и "expectedLocationId"
-            // они д.б. равны
-        returnedId, // значение, которое возвращает функция
-        expectedLocationId, // номер локации, за которую проголосовали (8)
-        "User was able to vote for the expected location and expectedVotedLocation should match what is returned."
-            // it's the message that we are going to display.
+                    // сравниваем переменные "returnedId" и "expectedLocationId"
+                    // они д.б. равны
+            returnedId, // значение, которое возвращает функция
+            expectedLocationId, // номер локации, за которую проголосовали (8)
+            "User was able to vote for the expected location and expectedVotedLocation should match what is returned."
         );
-
     }
 
-// Test Function #2
-  // compare the address in the blockchain of the location that was voted with the one that we are expecting.
-  // There is an error on this function, but we are going to figure it out when compiling the contract.
-  // Я ИЗМЕНИЛА ТУТ voting.location на voting.locations!
-
+// Test Function #2 - проверяем адрес
     function testGetVotedLocationAddressByLocationId() public {
-    // объявили тестовую функцию
+        // объявили тестовую функцию
 
-    // создали переменную location
         address location = voting.locations(expectedLocationId);
-        // address location - объявили переменную location, тип переменной = address
-        // voting.locations = переменная "locations" from contract "voting". Это массив.
-        // expectedLocationId (задано как 8)
-        // So, the location function is going to return the address in the blockchain for this location ID.
-        // (не поняла. Вернется ведь весь массив locations, а не 1 значение?)
+            // address location - объявили переменную location, тип переменной = address
+            // voting.location = ??? в контракте voting нет функции location, не понимаю на что ссылаемся
+            // expectedLocationId (задано как 8)
+            // "So, the location function is going to return the address in the blockchain for this location ID."
+            // (нет такой функции)
 
-    // сравниванием переменные
         Assert.equal(
             location, // значение созданной переменной
-            expectedVotedLocation, //
-            'Voted Location of the expected location should be this contract'
+            expectedVotedLocation, // address of the voted location
+            "Voted Location of the expected location should be this contract"
         );
-
-
     }
 
+// Test Function #3 - test the location address by location ID in the array.
+    function testGetVotedLocationAddressByLocationIdInArray() public {
+        // объявили тестовую функцию
 
+        address[16] memory locations = voting.getLocations();
+            // создали переменную locations, с типом address[16]
+            // memory -- Solidity will temporarily store the value in memory.
+            // voting.getLocations() -- функция getLocations из файла Voting. Она возвращает массив со списком проголосованных локаций
 
+        Assert.equal(
+            locations[expectedLocationId],
+                // взять локацию с id 8 (expectedLocationId) из всего массива проголосованных локаций (locations)
+            expectedVotedLocation,
+                // address of the voted location
+            "Voted location of the expected location should be this contract"
+        );
+    }
+}
